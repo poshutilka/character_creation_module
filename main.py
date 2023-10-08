9ca5efc71cfca56556d0514d86f63833e39ae7b2
Mikhail Alekseev <russki.poshutilka@yandex.ru>
2023-10-09 02:26:09 +0300

    Docstring added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+"""Это документация для добашнего задания."""
 from random import randint
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Описывает работу функции attack."""
     if char_class == 'warrior':
         return (
             f'{char_name} нанёс урон противнику равный {5 + randint(3, 5)}')
@@ -16,6 +18,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Описывает работу функции defence."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -27,6 +30,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Описывает работу функции special."""
     if char_class == 'warrior':
         return (
             f'{char_name} применил специальное умение'
@@ -39,6 +43,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Описывает работу функции start_training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -64,6 +69,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Описывает работу функции choice_char_class."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -83,21 +89,23 @@ def choice_char_class() -> str:
                 'Лекарь — могущественный заклинатель. Черпает'
                 'силы из природы, веры и духов.')
         approve_choice = input(
-            'Нажми (Y), чтобы подтвердить выбор, или любую другую'
+            'Нажми (Y), чтобы подтвердить выбор, или любую другую '
             'кнопку, чтобы выбрать другого персонажа ').lower()
     return str(char_class)
 
 
-def main():
-    print('Приветствую тебя, искатель приключений!')
+def main() -> None:
+    '''Описывает работу функции main.'''
+    run_screensaver()
+    print('Приветствую тебя, искатель приключений! ')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
-          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
-    print('Ты можешь выбрать один из трёх путей силы:')
+          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10. ')
+    print('Ты можешь выбрать один из трёх путей силы: ')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
-# main()
+main()
